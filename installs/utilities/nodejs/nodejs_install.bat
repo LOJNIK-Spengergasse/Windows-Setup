508809a878d7ea78615c0ed22db83411469a2d04
Lojnik <loj210680@spengergasse.at>
2024-12-06 09:33:14 +0100

    nodejs - lts & stable version

--- installs/utilities/nodejs/nodejs_install.bat
+++ installs/utilities/nodejs/nodejs_install.bat
@@ -9,7 +9,7 @@ if %errorlevel% neq 0 (
 
 :: Install scrcpy using winget
 echo Installing NodeJS via winget...
-winget install OpenJS.NodeJS :: NOT LTS
+winget install OpenJS.NodeJS :: STABLE VERSION, NOT LTS
 
 :: Confirm installation
 echo NodeJS installation complete. You can now run scrcpy from the command line.
